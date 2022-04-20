e1b5e26891c08d88eae06eae13461ede809ecef6
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-20 10:37:28 +0200

    change classnames in daily component

--- src/components/daily.js
+++ src/components/daily.js
@@ -8,22 +8,22 @@ const dailyData = ({dailyData}) => (
             {dailyData ? <p>{dailyData[1].temp.max.toFixed()}°</p> : null}
             {dailyData ? <img src={`http://openweathermap.org/img/wn/${dailyData[1].weather[0].icon}@2x.png`} alt="weather"></img> : null}
           </div>
-          <div className="humidity">
+          <div className="daily">
             {dailyData ? <p>{new Date(dailyData[2].dt * 1000).getMonth()+1 + "/" +new Date(dailyData[2].dt * 1000).getDate()}</p> : null}
             {dailyData ? <p>{dailyData[2].temp.max.toFixed()}°</p> : null}
             {dailyData ? <img src={`http://openweathermap.org/img/wn/${dailyData[2].weather[0].icon}@2x.png`} alt="weather"></img> : null}
           </div>
-          <div className="wind">
+          <div className="daily">
             {dailyData ? <p>{new Date(dailyData[3].dt * 1000).getMonth()+1 + "/" +new Date(dailyData[3].dt * 1000).getDate()}</p> : null}
             {dailyData ? <p>{dailyData[3].temp.max.toFixed()}°</p> : null}
             {dailyData ? <img src={`http://openweathermap.org/img/wn/${dailyData[3].weather[0].icon}@2x.png`} alt="weather"></img> : null}
           </div>
-          <div className="wind">
+          <div className="daily">
             {dailyData ? <p>{new Date(dailyData[4].dt * 1000).getMonth()+1 + "/" +new Date(dailyData[4].dt * 1000).getDate()}</p> : null}
             {dailyData ? <p>{dailyData[4].temp.max.toFixed()}°</p> : null}
             {dailyData ? <img src={`http://openweathermap.org/img/wn/${dailyData[4].weather[0].icon}@2x.png`} alt="weather"></img> : null}
           </div>
-          <div className="wind">
+          <div className="daily">
             {dailyData ? <p>{new Date(dailyData[5].dt * 1000).getMonth()+1 + "/" +new Date(dailyData[5].dt * 1000).getDate()}</p> : null}
             {dailyData ? <p>{dailyData[5].temp.max.toFixed()}°</p> : null}
             {dailyData ? <img src={`http://openweathermap.org/img/wn/${dailyData[5].weather[0].icon}@2x.png`} alt="weather"></img> : null}
