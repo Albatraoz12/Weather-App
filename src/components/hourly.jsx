75cbaf791a969b0c11d763245177fb25b66e2218
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-25 09:28:05 +0200

    add new hover effect to display icons on hourly

--- src/components/hourly.jsx
+++ src/components/hourly.jsx
@@ -8,47 +8,36 @@ import '../index.css';
 
 function HourlyData ({hourlyData}) {
   return(
-
-    // hourlyData.forEach((item, index) => {
-    //   index++;
-    //   if(index <= 5 ) {
-    //     //skriv ut html
-    //     // return (
-    //     <div className="bottom">
-    //     <div className="hourly">
-    //     {item ? <p>{new Date(item[index].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
-    //     {item ? <p>{item[index].temp.toFixed()}°</p> : null}
-    //     {item ? <img src={`http://openweathermap.org/img/wn/${item[index].weather[0].icon}@2x.png`} alt="weather"></img> : null}
-    //   </div>
-    //   </div>
-    //   // )
-    //   }
-    // })
    <div className="bottom">
    <div className="hourly">
     {hourlyData ? <p>{new Date(hourlyData[0].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
     {hourlyData ? <p>{hourlyData[0].temp.toFixed()}°</p> : null}
     {hourlyData ? <img src={`http://openweathermap.org/img/wn/${hourlyData[0].weather[0].icon}@2x.png`} alt="weather"></img> : null}
+    {hourlyData ? <span className='tooltiptext'>{hourlyData[0].weather[0].description}</span> : null}
   </div>
   <div className="hourly">
     {hourlyData ? <p>{new Date(hourlyData[1].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
     {hourlyData ? <p>{hourlyData[1].temp.toFixed()}°</p> : null}
     {hourlyData ? <img src={`http://openweathermap.org/img/wn/${hourlyData[1].weather[0].icon}@2x.png`} alt="weather"></img> : null}
+    {hourlyData ? <span className='tooltiptext'>{hourlyData[0].weather[0].description}</span> : null}
   </div>
   <div className="hourly">
     {hourlyData ? <p>{new Date(hourlyData[2].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
     {hourlyData ? <p>{hourlyData[2].temp.toFixed()}°</p> : null}
     {hourlyData ? <img src={`http://openweathermap.org/img/wn/${hourlyData[2].weather[0].icon}@2x.png`} alt="weather"></img> : null}
+    {hourlyData ? <span className='tooltiptext'>{hourlyData[0].weather[0].description}</span> : null}
   </div>
   <div className="hourly">
     {hourlyData ? <p>{new Date(hourlyData[3].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
     {hourlyData ? <p>{hourlyData[3].temp.toFixed()}°</p> : null}
     {hourlyData ? <img src={`http://openweathermap.org/img/wn/${hourlyData[3].weather[0].icon}@2x.png`} alt="weather"></img> : null}
+    {hourlyData ? <span className='tooltiptext'>{hourlyData[0].weather[0].description}</span> : null}
   </div>
   <div className="hourly">
     {hourlyData ? <p>{new Date(hourlyData[4].dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> : null}
     {hourlyData ? <p>{hourlyData[4].temp.toFixed()}°</p> : null}
     {hourlyData ? <img src={`http://openweathermap.org/img/wn/${hourlyData[4].weather[0].icon}@2x.png`} alt="weather"></img> : null}
+    {hourlyData ? <span className='tooltiptext'>{hourlyData[0].weather[0].description}</span> : null}
   </div> 
  </div> 
  )
