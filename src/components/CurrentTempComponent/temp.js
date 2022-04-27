5d420c75673e890a0b96d1a4a3f2ed29fee07f9e
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-27 20:54:35 +0200

    add icons to sunrise and sunset

--- src/components/CurrentTempComponent/temp.js
+++ src/components/CurrentTempComponent/temp.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './temp.css';
+import sunriseIcon from '../../assets/sunRiseIcon.png'
+import sunsetIcon from '../../assets/sunSetIcon.png'
+
 
 const temp = ({tempData, grade}) => (
   <div className="top">
   <div className="location">
     <h1>{tempData.timezone}</h1>
     <p>Time {new Date(tempData.current.dt * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p>
-    <p>Sunrise {new Date(tempData.current.sunrise * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p>
-    <p>Sunset {new Date(tempData.current.sunset * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> 
   </div>
   <div className="temp">
     <h2>{tempData.current.temp.toFixed()}°{grade}</h2>
@@ -16,11 +17,13 @@ const temp = ({tempData, grade}) => (
   <div className="description">
   <p>{tempData.current.weather[0].main}</p>
   </div>
-  <div className="location">
+  <div className="sundirection">
     <div>
+      <img src={sunriseIcon} alt="sun rise icon" />
       <p>Sunrise {new Date(tempData.current.sunrise * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p>
     </div>
     <div>
+      <img src={sunsetIcon} alt="sun rise icon" />
       <p>Sunset {new Date(tempData.current.sunset * 1000).toLocaleTimeString('sv-SE',{ hour: '2-digit', minute: '2-digit' })}</p> 
     </div>
   </div>
