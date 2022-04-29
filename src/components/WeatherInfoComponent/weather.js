d4af82fc5a7e792ec8298ed33c11153438f20317
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.se>
2022-04-29 18:36:49 +0200

    add icons to feelslike, windspeed and humidity

--- src/components/WeatherInfoComponent/weather.js
+++ src/components/WeatherInfoComponent/weather.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './weather.css';
+import FeelsLike from '../../assets/FeelsLike.png'
+import HumidityIcon from '../../assets/HumidityIcon.png'
+import WindSpeedIcon from '../../assets/WindSpeedIcon.png'
 
 const weather = ({weatherData, grade, speed}) => (
   <div className="bottom">
           <div className="feels">
+            <img src={FeelsLike} alt="temperture glass icon" />
             <p className="bold">{weatherData.feels_like.toFixed()}°{grade}</p>
             <p>Feels Like</p>
           </div>
           <div className="humidity">
+          <img src={HumidityIcon} alt="temperture glass icon" />
             <p className="bold">{weatherData.humidity}%</p>
             <p>Humidity</p>
           </div>
           <div className="wind">
+          <img src={WindSpeedIcon} alt="temperture glass icon" />
           <p className="bold">{weatherData.wind_speed.toFixed()} {speed}</p>
             <p>Wind Speed</p>
           </div>
